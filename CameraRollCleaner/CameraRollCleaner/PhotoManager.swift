43d87f197db1e8dbf97d1ddce546f049e9f2bb4c
Kendall Starcevich <kendall.starcevich@drake.edu>
2026-02-12 13:39:26 -0600

    screenshot detection functioning

--- CameraRollCleaner/CameraRollCleaner/PhotoManager.swift
+++ CameraRollCleaner/CameraRollCleaner/PhotoManager.swift
@@ -6,7 +6,7 @@ class PhotoManager: ObservableObject {
     @Published var photoCount = 0
     @Published var screenshotCount = 0
     @Published var isAuthorized = false
-
+    
     func requestAccessAndFetch() {
         PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
             DispatchQueue.main.async {
@@ -17,58 +17,63 @@ class PhotoManager: ObservableObject {
             }
         }
     }
-
-    private func fetchMetadata() {
-        let options = PHFetchOptions()
-        
-        // Sort by creation date (newest first)
-        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-        
-        // Note: PHFetchResult doesn't have a "limit" property,
-        // but we can limit our iteration later.
-        // However, we can set a fetch limit to save memory:
-        options.fetchLimit = 50
-
-        let allPhotos = PHAsset.fetchAssets(with: .image, options: options)
-        
-        self.photoCount = allPhotos.count
-    }
     
-}
-
-struct PhotoThumbnail: View {
-    let asset: PHAsset
-    @State private var image: UIImage? = nil
-
-    var body: some View {
-        Group {
-            if let image = image {
-                Image(uiImage: image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-            } else {
-                Rectangle()
-                    .fill(Color.gray.opacity(0.3))
-                    .onAppear {
-                        loadThumbnail()
-                    }
+    private func fetchMetadata() {
+        let allPhotos = PHAsset.fetchAssets(with: .image, options: nil)
+        self.photoCount = allPhotos.count
+        
+        // 1. Create a dynamic array to hold our findings
+        var foundScreenshots = 0
+        
+        // 2. Loop through the photos and check for signs of a screenshot
+        allPhotos.enumerateObjects { (asset, index, stop) in
+            // Check A: The official Apple Subtype
+            let isOfficialScreenshot = asset.mediaSubtypes.contains(.photoScreenshot)
+            
+            // Check B: The "Simulator" check (Simulators often omit the subtype)
+            // We can check if the metadata suggests it's a screenshot
+            if isOfficialScreenshot {
+                foundScreenshots += 1
             }
         }
-        .frame(width: 100, height: 100)
-        .clipped()
+        
+        self.screenshotCount = foundScreenshots
     }
-
-    func loadThumbnail() {
-        let manager = PHImageManager.default()
-        let options = PHImageRequestOptions()
-        options.isSynchronous = false // Don't freeze the app!
-        options.deliveryMode = .opportunistic
-
-        manager.requestImage(for: asset,
-                             targetSize: CGSize(width: 200, height: 200),
-                             contentMode: .aspectFill,
-                             options: options) { result, _ in
-            self.image = result
+    
+    struct PhotoThumbnail: View {
+        let asset: PHAsset
+        @State private var image: UIImage? = nil
+        
+        var body: some View {
+            Group {
+                if let image = image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } else {
+                    Rectangle()
+                        .fill(Color.gray.opacity(0.3))
+                        .onAppear {
+                            loadThumbnail()
+                        }
+                }
+            }
+            .frame(width: 100, height: 100)
+            .clipped()
+        }
+        
+        func loadThumbnail() {
+            let manager = PHImageManager.default()
+            let options = PHImageRequestOptions()
+            options.isSynchronous = false // Don't freeze the app!
+            options.deliveryMode = .opportunistic
+            
+            manager.requestImage(for: asset,
+                                 targetSize: CGSize(width: 200, height: 200),
+                                 contentMode: .aspectFill,
+                                 options: options) { result, _ in
+                self.image = result
+            }
         }
     }
 }
